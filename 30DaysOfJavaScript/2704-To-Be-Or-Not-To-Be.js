11d4a5d8bf71be67580dc1d2f1860f242c52843f
Emilia Nodzewska <114150876+emilia-nodz@users.noreply.github.com>
2025-02-09 12:31:28 +0100

    [2704] [Time Beats: 80.57%] [Memory Beats: 43.57%] - LeetPush

---
+++ 30DaysOfJavaScript/2704-To-Be-Or-Not-To-Be.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    let object = {
+        toBe: function(value) {
+            if (val === value) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+
+        notToBe: function(value) {
+            if(val !== value) {
+                return true
+            } else {
+                throw new Error("Equal");
+            }
+        } 
+    };
+
+    return object
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
